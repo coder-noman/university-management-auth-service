b13563090915f8df08e2888cee1ed99a47d706a1
Abdhulla Al Noman <86647135+coder-noman@users.noreply.github.com>
2023-06-19 21:54:13 +0600

    hello

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
-const port = 3000
 
 app.use(cors())
 app.use(express.json())
@@ -11,5 +10,4 @@ app.get('/', (req: Request, res: Response) => {
   res.send('working successfully')
 })
 
-export default app;
-
+export default app
