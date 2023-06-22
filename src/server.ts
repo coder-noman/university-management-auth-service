bcbd69d86473b5e185e05b9d2e8606881b8c1edb
Abdhulla Al Noman <86647135+coder-noman@users.noreply.github.com>
2023-06-22 22:30:20 +0600

    [DONE] Logger setup

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
+import { errorlogger, logger } from './shared/logger'
 
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('Database is connected succesfully')
+    logger.info('Database is connected succesfully')
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`)
+      logger.info(`Application listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log('failed to connect database', err)
+    errorlogger.error('failed to connect database', err)
   }
 }
 
